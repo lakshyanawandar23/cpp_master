7a8e6353c33f46466e03ee627a4076925ab9f69e
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2023-06-23 12:30:12 +0530

    Time: 88 ms (8.48%), Space: 18.9 MB (79.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& p, int t) {
-        int l=1,h,ans=INT_MAX;
-        for(auto it:p){
-            h=max(h,it);
+   long long int solve(vector<int>&p,long long int &m){
+     long long   int sum=0;
+        for(int i=0;i<p.size();i++){
+            if(p[i]%m==0){
+                sum+=(p[i]/m);
+            }
+            else {
+   sum+=(p[i]/m+1);
+            }
         }
-        while(l<=h){
-            int m=(l+h)/2;
-        long long  int cnt=0;
-            for(int i=0;i<p.size();i++){
-                if(p[i]%m==0){
-                    cnt+=(p[i]/m);
-                }
-                else{
-                    cnt+=(p[i]/m+1);
-                }
+        return sum;
+    }
+    int minEatingSpeed(vector<int>& p, int h) {
+       int low=1,high;
+      long long  int ans=1e9;
+        for(auto it:p){
+            high=max(high,it);
+        }
+        while(low<=high){
+          long long  int m=(long long int )(low+high)/2;
+       long long  int res=solve(p,m);
+            if(res<=h){
+                ans=min(ans,m);
+                    high=m-1;
             }
-            if(cnt<=t){
-                ans=m;
-                h=m-1;
-            }
-            else{
-                l=m+1;
+            else {
+               low=m+1;         
             }
         }
         return ans;
