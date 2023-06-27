94f3595b1e5583e82b397a6dec451f35441a64c2
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2023-06-27 10:33:34 +0530

    Time: 553 ms (28.26%), Space: 210.4 MB (6.59%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-   static bool cmp(pair<int,int>&a,pair<int,int>&b){
+    static bool help(pair<int,int>&a,pair<int,int>&b){
         if(a.first==b.first){
-           return a.second>b.second;
+            return a.second>b.second;
         }
-        return a.first<b.first;
+        return  a.first<b.first;
     }
-    int solve(int i,vector<pair<int,int>>&v,vector<vector<int>>&dp,int prev){
+    int solve(vector<pair<int,int>>&v,int i,int prev,vector<vector<int>>&dp){
         if(i==v.size()){
             return 0;
         }
         if(dp[i][prev]!=-1){
             return dp[i][prev];
         }
-        int no=0+solve(i+1,v,dp,prev);
-        int select=0;
-        if(prev<=v[i].second){
-            select=v[i].first+solve(i+1,v,dp,v[i].second);
+        int no=0+solve(v,i+1,prev,dp);
+        int pick=0;
+        if(v[i].second>=prev){
+            pick=v[i].first+solve(v,i+1,v[i].second,dp);
         }
-        return dp[i][prev]=max(no,select);
+        return dp[i][prev]=max(no,pick);
     }
     int bestTeamScore(vector<int>& s, vector<int>& a) {
-        int n=s.size(),mx=0;
         vector<pair<int,int>>v;
-        for(int i=0;i<n;i++){
+        int mx=0;
+        for(int i=0;i<s.size();i++){
             mx=max(mx,a[i]);
             v.push_back({s[i],a[i]});
         }
         sort(v.begin(),v.end());
+        int n=s.size();
         vector<vector<int>>dp(n,vector<int>(mx+1,-1));
-        return solve(0,v,dp,0);
+        return solve(v,0,0,dp);
     }
 };
