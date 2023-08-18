ba9aa0155b100cf486c09d1ff87f34ac3da826ea
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2023-08-18 11:13:28 +0530

    Time: 73 ms (81.12%), Space: 33.6 MB (53.03%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& r) {
+        vector<int>adj[n];
+        for(auto it:r){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+         //   set<int>s;
+           // set<int>original;
+            int cnt=0;
+        for(auto it:adj[i]){
+          //  s.insert(i);
+        //original.insert(i);
+            cnt++;
+        }
+            for(int j=i+1;j<n;j++){
+                int res=0;
+                for(auto it:adj[j]){
+               if(it!=i){
+                   res++;
+               }
+                }
+            ans=max(ans,res+cnt);
+           // s.clear();
+        //    s.assign(original);
+            //s.insert(original.begin(),original.end());
+        }
+        }
+        return ans;
+    }
+};
