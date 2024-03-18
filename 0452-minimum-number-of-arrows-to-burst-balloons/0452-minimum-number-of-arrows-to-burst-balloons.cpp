b1d4830fae6ba5276ee7b7e9ed2a4294bf43e7fb
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2024-03-18 15:24:40 +0530

    Time: 276 ms (31.27%), Space: 112.6 MB (12.60%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     static bool cmp(pair<int,int>a,pair<int,int>b){
-        if(a.second==b.second){
-            return a.first<b.first;
-        }
+        if(a.second==b.second) return a.first<b.first;
         return a.second<b.second;
     }
-    int findMinArrowShots(vector<vector<int>>& s) {
+    int findMinArrowShots(vector<vector<int>>& p) {
+        int n=p.size();
         vector<pair<int,int>>v;
-        for(int i=0;i<s.size();i++){
-            v.push_back({s[i][0],s[i][1]});
+        for(auto it:p){
+            v.push_back({it[0],it[1]});
         }
         sort(v.begin(),v.end(),cmp);
-        int cnt=1,st=0;
-        for(int i=1;i<v.size();i++){
-             if(v[i].first>v[st].second){
-                  cnt++;
-                 st=i;
-             }
+        int cnt=1,prev=v[0].second;
+        for(int i=1;i<n;i++){
+            if(prev<v[i].first){
+                cnt++;
+                prev=v[i].second;
+            }
         }
         return cnt;
     }
