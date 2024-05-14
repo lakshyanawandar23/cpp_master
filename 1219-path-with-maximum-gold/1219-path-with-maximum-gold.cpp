6f2e547c76c23b6391e479299923a706b5b7c0d2
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2024-05-14 11:47:06 +0530

    Time: 265 ms (28.02%), Space: 13.4 MB (9.87%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int dx[4]={1,-1,0,0};
-    int dy[4]={0,0,1,-1};
-    int solve(int i,int j,vector<vector<int>>&vis,vector<vector<int>>&g,int n,int m){
-      if(i<0||j<0||i==n||j==m||vis[i][j]==1){
-            return 0;
-        }
-        if(g[i][j]==0){
-            return INT_MIN;
-        }
+    int ans;
+    int solve(vector<vector<int>>&g,vector<vector<int>>&vis,int i,int j,int &n ,int &m){
+        if(i<0||j<0||i>=n||j>=m||vis[i][j]==1||g[i][j]==0) return 0;
         vis[i][j]=1;
-        int ans=0;
-        for(int k=0;k<4;k++){
-            int x=i+dx[k];
-            int y=j+dy[k];
-                ans=max(ans,g[i][j]+solve(x,y,vis,g,n,m));
-        }
+        int l=solve(g,vis,i,j-1,n,m);
+        int r=solve(g,vis,i,j+1,n,m);
+        int d=solve(g,vis,i+1,j,n,m);
+        int u=solve(g,vis,i-1,j,n,m);
         vis[i][j]=0;
-        return ans;
+        return  g[i][j]+max({l,r,u,d});
     }
     int getMaximumGold(vector<vector<int>>& g) {
-        int n=g.size(),m=g[0].size(),ans=0;
-                     vector<vector<int>>vis(n,vector<int>(m,0));
+        int n=g.size(),m=g[0].size();
+        ans=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(g[i][j]!=0){
-                    ans=max(ans,solve(i,j,vis,g,n,m));
-                }
-            }
+          for(int j=0;j<m;j++){
+              if(g[i][j]>=0){
+                  vector<vector<int>>vis(n,vector<int>(m,0));
+                  ans=max(ans,solve(g,vis,i,j,n,m));
+              }
+          }
         }
         return ans;
     }
