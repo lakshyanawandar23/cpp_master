11e3fd5050f679e29e05ace87b85cb49c915bca7
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2024-09-06 15:21:00 +0530

    Time: 88 ms (82.73%), Space: 126.4 MB (8.52%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -5,13 +5,13 @@ public:
         long long int sum=0,rem=0;
         for(auto it:s) sum+=it;
         rem=(m*(n+s.size())-sum);
-        if((6*n)<rem) return {};
+        if((6*n)<rem||rem<n) return {};
         int i=0;
         while(i<n){
             int a=rem/(n-i);
            // int carry=rem%(n-i);
             rem=(rem-a);
-            if(a>6||a<=0) return {};
+         //   if(a>6||a<=0) return {};
             v.push_back(a);
             i++;
         }
