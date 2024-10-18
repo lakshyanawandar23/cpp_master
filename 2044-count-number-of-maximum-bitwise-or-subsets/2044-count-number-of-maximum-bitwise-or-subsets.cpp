fc782cf80ab051cc2a51b261c74076a8e9c8c109
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2024-10-18 10:25:52 +0530

    Time: 10 ms (83.65%), Space: 10.2 MB (48.34%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -3,9 +3,10 @@ public:
     int ans;
     void solve(vector<int>&num,int i,int &x,int curr){
         if(i==num.size()) return ;
-        int t=curr|num[i];
-    if(t==x) ans++;
+      
         solve(num,i+1,x,curr);
+          int t=curr|num[i];
+    if(t==x) ans++;
         solve(num,i+1,x,t);
         return ;
     }
