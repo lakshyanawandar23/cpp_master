b753ee935ab0bacb908d55ce5edc3d84b1936751
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2024-12-13 09:46:25 +0530

    Time: 75 ms (79.78%), Space: 106.3 MB (48.04%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
+    static bool cmp(pair<int,int>&a,pair<int,int>&b){
+        if(a.first==b.first) return a.second<b.second;
+        return a.first<b.first;
+    }
     long long findScore(vector<int>& num) {
-        long long int cnt=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        long long int ans=0;
+        vector<pair<int,int>>v;
         for(int i=0;i<num.size();i++){
-            q.push({num[i],i});
+            v.push_back({num[i],i});
         }
-        set<int>s;
-        while(!q.empty()){
-            auto p=q.top();
-            q.pop();
-            if(s.find(p.second)==s.end()){
-                s.insert(p.second);
-                cnt+=p.first;
-                s.insert(p.second-1);
-                s.insert(p.second+1);
+        sort(v.begin(),v.end(),cmp);
+        for(int i=0;i<v.size();i++){
+            int idx=v[i].second;
+            if(num[v[i].second]>0){
+                ans+=v[i].first;
+                if((idx-1)>=0) num[idx-1]=-1;
+                if((idx+1)<num.size()) num[idx+1]=-1;
             }
         }
-        return cnt;
+        return ans;
     }
 };
