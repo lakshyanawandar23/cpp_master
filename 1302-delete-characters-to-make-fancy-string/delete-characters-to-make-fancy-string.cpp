7b6aaab4d3cbfd9317b9f3aec719238ed951595c
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2025-07-21 07:38:27 +0530

    Time: 112 ms (7.19%) | Memory: 52.7 MB (5.59%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-           unordered_map<int,int>mp;
-           int cnt=1;
-           for(int i=1;i<s.size();i++){
-            if(s[i]==s[i-1]){
-                cnt++;
-                if(cnt>=3){
-                    mp[i]++;
+        stack<char>c;
+        for(int i=0;i<s.size();i++){
+            if(c.size()>=2&&c.top()==s[i]){
+                auto c1=c.top();
+                c.pop();
+                if(c.top()!=s[i]){
+                    c.push(c1);
+                    c.push(s[i]);
                 }
+                else {
+                    c.push(c1);
+                }
+                
             }
             else {
-                cnt=1;
+                c.push(s[i]);
             }
-           }
-           string ans;
-           for(int i=0;i<s.size();i++){
-                     if(mp.find(i)==mp.end()) ans+=s[i];
-           }
-           return ans;
+        }
+        string ans;
+        while(!c.empty()){
+            ans+=c.top();
+            c.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
