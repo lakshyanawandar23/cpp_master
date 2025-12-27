8bf464f2a9bd275a900003aa3b6a2d22c24b7ad0
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2025-12-27 16:58:42 +0530

    Time: 66 ms (77.85%) | Memory: 98.1 MB (70.22%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -21,20 +21,14 @@ public:
             }
             else {
                long long int time=q2.top().first;
-            
-              //  int time2=v[i][0];
-               // time=max(time,time2);
-            //   //  cout<<time<<endl;
-            //     while(!q2.empty()&&q2.top().first<=time){
-            //         q.push(q2.top().second);
-            //         q2.pop();
-            //     }
-             auto it=q2.top().second;
-             q2.pop();
-                q2.push({1LL*(v[i][1]-v[i][0])+time,it});
-                ans[it]++;
+                int room=q2.top().second;
+                q2.pop();
+                q2.push({1LL*(v[i][1]-v[i][0])+time,room});
+                ans[room]++;
             }
         }
+    
+        
         int res=-1,maxi=0;
         for(int i=0;i<n;i++){
            // cout<<ans[i]<<endl;
