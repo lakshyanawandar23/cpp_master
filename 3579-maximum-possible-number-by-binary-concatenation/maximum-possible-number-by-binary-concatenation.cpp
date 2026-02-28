c9672d14cc5b7f05f3c941a5148cd8b7168bfcda
Lakshya <111570269+lakshyanawandar23@users.noreply.github.com>
2026-02-28 14:11:33 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (81.25%) - LeetSync

--- 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.cpp
+++ 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int maxGoodNumber(vector<int>& num) {
-        int f=0;
-        vector<string>v;
-        for(int i=0;i<num.size();i++){
-            string a;
-         while(num[i]>0){
-             string b=to_string(num[i]%2);
-             a=b+a;
-             num[i]/=2;
-         }
-            v.push_back(a);
-        }
-        sort(v.begin(),v.end());
+        int ans=0;
+        sort(num.begin(),num.end());
         do{
-        string res;
-        for(int i=0;i<v.size();i++){
-            //cout<<v[i]<<endl;
-            res+=v[i];
-        }
-        int i=res.size()-1,ans=0,j=0;
-        while(i>=0){
-            if(res[i]=='1') ans+=pow(2,j);
-            i--;
-            j++;
-        }
-           // cout<<ans<<endl;
-            f=max(f,ans);
-        }while(next_permutation(v.begin(),v.end()));
-        return f;
+            int res=0;
+            for(int i=0;i<3;i++){
+                int digit=log2(num[i])+1;
+                  res=(res<<digit)+num[i];
+            }
+            ans=max(ans,res);
+        }while(next_permutation(num.begin(),num.end()));
+        return ans;
     }
 };
